a31284a1f47769f5acae1895d214f2c942055a4a
DevelopSanchtz <armando.sanchtz@gmail.com>
2021-04-23 13:29:05 -0500

    proyecto subir laravel a heroku

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PRACTICA DE BASE DE DATOS MÓVILES</h1>
+    <h2>SUBIR PROYECTO LARAVEL A HEROKU</h2>
+
+    <h3>EQUIPO MIGUEL EDGARDO SANTIAGO Y JOSÉ ARMANDO TOVAR</h3>
+</body>
+</html>
